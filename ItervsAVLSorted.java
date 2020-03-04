8fa4512030b146c5ad46bb23b55b104e8a22fcba
ak2253 <61290235+ak2253@users.noreply.github.com>
2020-03-04 15:30:12 -0500

    Update ItervsAVLSorted.java

--- ItervsAVLSorted.java
+++ ItervsAVLSorted.java
@@ -3,13 +3,28 @@ public class ItervsAVLSorted {
 		AVLIter btree=new AVLIter();
 		BSTIter itree=new BSTIter();
 		Integer[] ar=CnSArray.getSortedArray(10000);
+		long start=System.currentTimeMillis();
 		for(int i=0;i<ar.length;i++) {
 			btree.insertIter(ar[i]);
 		}
+		System.out.println("Balance tree max depth: "+btree.maxdepth());
+		for(int i=0;i<ar.length;i++) {
+			btree.deleteIter(ar[i]);
+		}
+		long end=System.currentTimeMillis();
+		long AVLtime=end-start;
+		start=System.currentTimeMillis();
 		for(int i=0;i<ar.length;i++) {
 			itree.insertIter(ar[i]);
 		}
-		System.out.println("Balance tree max depth: "+btree.maxdepth());
 		System.out.println("Normal tree max depth: "+itree.maxdepth());
+		for(int i=0;i<ar.length;i++) {
+			itree.deleteIter(ar[i]);
+		}
+		end=System.currentTimeMillis();
+		long itertime=end-start;
+		System.out.println("Took time for Balance tree: "+AVLtime+" ms");
+		System.out.println("Took time for Normal tree: "+itertime+" ms");
+		System.out.println("\ntime difference: "+(itertime-AVLtime)+" ms");
 	}
 }
